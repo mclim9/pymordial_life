e643e292c18851e32ae40a54b2829796b829f3d8
Tinker Pad <mclim@stuff.com>
2017-09-14 21:14:45 -0700

    Biots now rotate and have limbs.  Rotate extra when they bump.

--- main.py
+++ main.py
@@ -38,35 +38,44 @@ Colors = [RED,(255,163,0),(255,255,0),GREEN,BLUE,WHITE]
 class Biot:    
    ###Class to keep track of a ball's location and vector.
    def __init__(self):
-      self.x = random.randrange(BALL_SIZE, ScrWid - BALL_SIZE)
-      self.y = random.randrange(BALL_SIZE, ScrHeight - BALL_SIZE)
+      self.size  = BALL_SIZE
+      self.x = random.randrange(self.size, ScrWid - self.size)
+      self.y = random.randrange(self.size, ScrHeight - self.size)
       self.color = random.choice(Colors)
       #		self.speed = random.randrange(1, 3)
       self.speed = 3
-      self.angle = random.uniform(0, math.pi*2)
-      self.size  = BALL_SIZE
+      self.angleMove = random.uniform(0, math.pi*2)
+      self.angleRot = random.uniform(0, math.pi*2)
+      self.symmetry = random.randint(3, 9)
 	  
    def move(self):
-      self.x += int(math.sin(self.angle) * self.speed)
-      self.y -= int(math.cos(self.angle) * self.speed)
+      self.x += int(math.sin(self.angleMove) * self.speed)
+      self.y -= int(math.cos(self.angleMove) * self.speed)
+
    def bounce(self):
       if self.x > ScrWid - self.size:
          self.x = 2*(ScrWid - self.size) - self.x
-         self.angle = - self.angle
+         self.angleMove = - self.angleMove
 
       elif self.x < self.size:
          self.x = 2*self.size - self.x
-         self.angle = - self.angle
+         self.angleMove = - self.angleMove
 
       if self.y > ScrHeight - self.size:
          self.y = 2*(ScrHeight - self.size) - self.y
-         self.angle = math.pi - self.angle
+         self.angleMove = math.pi - self.angleMove
 
       elif self.y < self.size:
          self.y = 2*self.size - self.y
-         self.angle = math.pi - self.angle
+         self.angleMove = math.pi - self.angleMove
+         
    def draw(self, screen):
-      pygame.draw.circle(screen, self.color, [int(self.x),int(self.y)], self.size, 4)
+      self.angleRot += 0.03
+      #pygame.draw.circle(screen, self.color, [int(self.x),int(self.y)], self.size, 4)
+      for i in range(0,self.symmetry):
+         newX = int(self.x + self.size * math.sin(self.angleRot + (2 * math.pi * i)/self.symmetry))
+         newY = int(self.y + self.size * math.cos(self.angleRot + (2 * math.pi * i)/self.symmetry))
+         pygame.draw.lines(screen, self.color, False, [(self.x,self.y), (newX, newY)], 2)
 
 def collide(p1, p2):
    dx = p1.x - p2.x
@@ -77,8 +86,8 @@ def collide(p1, p2):
       tangent = math.atan2(dy, dx)
       angle = 0.5 * math.pi + tangent
 
-      angle1 = 2*tangent - p1.angle
-      angle2 = 2*tangent - p2.angle
+      angle1 = 2*tangent - p1.angleMove
+      angle2 = 2*tangent - p2.angleMove
       speed1 = p2.speed*elasticity
       speed2 = p1.speed*elasticity
       if speed1 > 5:
@@ -86,13 +95,15 @@ def collide(p1, p2):
       if speed2 > 5:
          speed1 = 5
 
-      (p1.angle, p1.speed) = (angle1, speed1)
-      (p2.angle, p2.speed) = (angle2, speed2)
+      (p1.angleMove, p1.speed) = (angle1, speed1)
+      (p2.angleMove, p2.speed) = (angle2, speed2)
 
       p1.x += math.sin(angle)
       p1.y -= math.cos(angle)
       p2.x -= math.sin(angle)
       p2.y += math.cos(angle)
+      p1.angleRot +=  .6
+      p2.angleRot +=  .6
 
 def findBiot(biots, x, y):
     for p in biots:
