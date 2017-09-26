c1ff4b24104066f97796edbdbe62977a9cd5ebce
Tinker Pad <mclim@stuff.com>
2017-09-25 20:58:38 -0700

    added space

--- PymordialLife/profilr.py
+++ PymordialLife/profilr.py
@@ -4,5 +4,6 @@ import pstats
 
 cProfile.run('biiots.main()','profilr.txt')
 p = pstats.Stats('profilr.txt')
+
 #p.sort_stats('cumulative').print_stats(10)
 p.strip_dirs().sort_stats('cumulative').print_stats(10)
