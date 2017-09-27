e6971bc71651d286e69f96298da3478ecab97d22
mclim <martin.lim@rsa.rohde-schwarz.com>
2017-09-27 10:49:46 -0700

    changed output to *.dat

--- PymordialLife/profilr.py
+++ PymordialLife/profilr.py
@@ -2,7 +2,7 @@ import cProfile
 import biiots
 import pstats
 
-cProfile.run('biiots.main()','profilr.txt')
-p = pstats.Stats('profilr.txt')
+cProfile.run('biiots.main()','profilr.dat')
+p = pstats.Stats('profilr.dat')
 #p.sort_stats('cumulative').print_stats(10)
 p.strip_dirs().sort_stats('cumulative').print_stats(10)
