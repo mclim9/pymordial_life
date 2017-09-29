72a2432ec3b707a3de3f34655c5da8465acf6b28
Tinker Pad <mclim@stuff.com>
2017-09-28 21:42:45 -0700

    chg profilr.py to call Pymordial.py

--- PymordialLife/profilr.py
+++ PymordialLife/profilr.py
@@ -1,8 +1,8 @@
 import cProfile
-import biiots
+import Pymordial
 import pstats
 
-cProfile.run('biiots.main()','profilr.dat')
+cProfile.run('Pymordial.main()','profilr.dat')
 p = pstats.Stats('profilr.dat')
 #p.sort_stats('cumulative').print_stats(10)
 p.strip_dirs().sort_stats('cumulative').print_stats(10)
